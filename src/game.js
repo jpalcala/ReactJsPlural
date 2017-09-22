22f0076a195ea80a3238aee24726852e260d4c8a
Juan Alcala <juan.alcala@unosquare.com>
2017-09-22 15:47:32 -0500

    added game interface structure

--- src/game.js
+++ src/game.js
@@ -1,19 +1,41 @@
-
-const Starts = (props)=>{
+import React, { Component } from 'react';
+const Stars = (props)=>{
     return (
+        <div className='col-5'>
+            <i className='fa fa-star'></i>
+            <i className='fa fa-star'></i>
+            <i className='fa fa-star'></i>
+            <i className='fa fa-star'></i>
+
+        </div>
+    );
+}
+const Button = (props) => {
+    return(
         <div>
+            <button>=</button>
+        </div>
+    );
+}
+const Answer = (props)=>{
+    return(
+        <div className='col-5'>
             ...
         </div>
     );
 }
-
 class Game extends React.Component{
 
     render(){
         return(
 
-            <div>
+            <div >
                 <h3>Play Nine</h3>
+                <div className='row'>
+                    <Stars/>
+                    <Button/>
+                    <Answer/>
+                </div>
             </div>
         );
     }
